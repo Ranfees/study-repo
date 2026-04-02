d5874a28ee34f91b7ea27281327462f7960db730
Ranfees <153532316+Ranfees@users.noreply.github.com>
2026-04-02 23:46:12 +0530

    views folder

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express();
 const PORT = 3000;
 
-app.get('')
+app.get('/',(req,res)=>{
+    return res.send('hi')
+})
 app.listen(PORT,()=>{
-    console.log('app is running on ${PORT}')
+    console.log('app is running on 3000')
 })
